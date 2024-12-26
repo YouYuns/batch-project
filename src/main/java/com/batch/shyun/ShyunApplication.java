17db88eff7a3c8cdda17ea0e3174f40cfdddb342
shyun <tjdgh0312@naver.com>
2024-12-26 10:07:38 +0900

    배치 정산프로그램

---
+++ src/main/java/com/batch/shyun/ShyunApplication.java
@@ -0,0 +1,13 @@
+package com.batch.shyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/shyun/ShyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.shyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
